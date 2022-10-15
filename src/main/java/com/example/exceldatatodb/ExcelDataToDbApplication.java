40c60b1ed17d06794d27bc4ad75ffd36ff9e2662
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-16 00:13:54 +0530

    Project Template

---
+++ src/main/java/com/example/exceldatatodb/ExcelDataToDbApplication.java
@@ -0,0 +1,14 @@
+package com.example.exceldatatodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDataToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDataToDbApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/example/exceldatatodb/ExcelDataToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceldatatodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDataToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
