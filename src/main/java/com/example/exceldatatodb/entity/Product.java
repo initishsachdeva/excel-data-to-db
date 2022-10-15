e12ec5301116bbd73e0b3ba47bf68501126b7e8f
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-16 02:04:38 +0530

    Entity class

---
+++ src/main/java/com/example/exceldatatodb/entity/Product.java
@@ -0,0 +1,19 @@
+package com.example.exceldatatodb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private Integer productId;
+    private String productName;
+    private String productDesc;
+    private Double productPrice;
+}
